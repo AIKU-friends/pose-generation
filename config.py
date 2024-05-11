e1e914054f23c717d804b6dbad7755acd11eae95
PROLCY <ckd248@naver.com>
2024-05-11 11:59:11 +0000

    experiment 28 commit

--- config.py
+++ config.py
@@ -9,15 +9,15 @@ class Config:
         self.fc_dim = 512
         self.latent_dim = 30
         self.sd_dim = 34
-        self.class_weight = [0.6072, 0.32255, 1.45106, 1.69682, 2.15381, 0.6999, 1.21317, 1.09274, 2.76061, 0.79155, 1.97931, 1.76047, 1.08609, 1.38459]
+        self.class_weight = [0.90988, 0.48819, 0.68802, 1.79232, 1.78693, 2.49072, 1.55075, 1.24016, 1.15953, 2.0858, 2.0858, 0.36321, 1.44285, 0.86111]
 
         self.target_point_method = 'mean' # ['mean', 'center', keypoint_index]
-        self.data_tag = '_kmeans_2'
+        self.data_tag = '_kmeans_30_filtered_14'
 
         self.num_epochs = 1000
         self.validation_term = 1
-        self.variational_beta = 5
-        self.learning_rate = 4e-4
+        self.variational_beta = 10
+        self.learning_rate = 2e-4
         self.adam_beta1 = 0.5
         self.adam_beta2 = 0.999
         self.weight_decay = 2e-5
@@ -28,7 +28,7 @@ class Config:
 
         self.backbone_freeze = False
 
-        self.checkpoint_dir = 'checkpoints/experiment26'
+        self.checkpoint_dir = 'checkpoints/experiment28'
         if not os.path.exists('checkpoints'):
             os.mkdir('checkpoints')
         if not os.path.exists(self.checkpoint_dir):
