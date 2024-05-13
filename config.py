2167ae77831b2dc968d47381fe158c6b7e05cffe
PROLCY <ckd248@naver.com>
2024-05-13 01:43:01 +0000

    experiment 30 commit

--- config.py
+++ config.py
@@ -3,32 +3,37 @@ import os
 class Config:
     def __init__(self):
         
-        self.alexnet_fc7_dim = 4096
-        self.pose_dim = 14
+        self.backbone = 'ResNetUp' # ['AlexNet', 'VGG', 'ResNetUp']
+        if self.backbone == 'AlexNet':
+            self.backbone_feature_dim = 4096
+        if self.backbone == 'VGG':
+            self.backbone_feature_dim = 25088
+        if self.backbone == 'ResNetUp':
+            self.backbone_feature_dim = 100352
+        self.pose_dim = 30
 
         self.fc_dim = 512
         self.latent_dim = 30
         self.sd_dim = 34
-        self.class_weight = [0.90988, 0.48819, 0.68802, 1.79232, 1.78693, 2.49072, 1.55075, 1.24016, 1.15953, 2.0858, 2.0858, 0.36321, 1.44285, 0.86111]
-
+        self.class_weight = [2.74664, 0.99431, 1.12161, 1.60353, 1.97212, 0.97565, 1.65784, 1.85766, 1.24029, 1.90442, 1.1788, 1.46778, 0.63529, 0.58117, 2.16727, 1.46006, 0.43825, 1.11113, 0.38999, 1.14633, 0.79716, 0.80799, 2.01998, 1.51867, 1.08789, 0.97565, 0.47692, 1.44736, 1.63183, 0.65324]
         self.target_point_method = 'mean' # ['mean', 'center', keypoint_index]
-        self.data_tag = '_kmeans_30_filtered_14'
+        self.data_tag = '_kmeans_1'
 
         self.num_epochs = 1000
         self.validation_term = 1
         self.variational_beta = 10
-        self.learning_rate = 4e-4
+        self.learning_rate = 2e-4
         self.adam_beta1 = 0.5
         self.adam_beta2 = 0.999
         self.weight_decay = 2e-5
         self.CLIP = 1
-        self.batch_size =  256
+        self.batch_size =  128
         
         self.pck_threshold = 0.2
 
         self.backbone_freeze = False
 
-        self.checkpoint_dir = 'checkpoints/experiment29'
+        self.checkpoint_dir = 'checkpoints/experiment30'
         if not os.path.exists('checkpoints'):
             os.mkdir('checkpoints')
         if not os.path.exists(self.checkpoint_dir):
